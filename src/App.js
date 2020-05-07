b247cc90403fb41f21216e323153178d1a50168a
Shir Brass <shirbr510@gmail.com>
2020-05-07 22:06:07 +0300

    created a very basic video player without any context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import MediaPlayer from "./components/MediaPlayer";
 
 export default function App() {
+  const [media, setMedia] = useState(null);
+  const setVideo=()=>setMedia({
+    format: 'video',
+    src: "http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"
+  })
+  const setImage = () => setMedia({
+    format: 'image',
+    src: "https://source.unsplash.com/random/300x200"
+  })
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <MediaPlayer
-        format={"video"}
-        src={"http://techslides.com/demos/sample-videos/small.mp4"}
-      />
+      <button onClick={setVideo}>Video</button>
+      <button onClick={setImage}>Image</button>
+      { media && <MediaPlayer {...media} /> }
     </div>
   );
 }
--- src/components/MediaPlayer.js
+++ src/components/MediaPlayer.js
@@ -1,19 +1,99 @@
-import React from "react";
+import React, { useRef, useCallback,useEffect, useState} from "react";
+import "./MediaPlayer.css";
 
-const Media = ({ format, alt, ...props }) => {
+const stringifyTime = (timeInSeconds = 0) => {
+  const time = Math.floor(timeInSeconds);
+  const timeInMinutes = Math.floor(time / 60)
+  const restSeconds = time % 60
+  return `${timeInMinutes}:${restSeconds}`
+}
+
+const Media = ({ time, format, alt, isPlaying, onTimeUpdate, ...props }) => {
+  const imageRef = useRef(null);
+  const videoRef = useRef(null);
+  useEffect(()=>{
+    const currentVideo = videoRef.current;
+    if(currentVideo){
+      if (isPlaying) {
+        currentVideo.play()
+      }
+      else {
+        currentVideo.pause()
+      }
+    }
+    
+  },[isPlaying])
+
+  useEffect(() => {
+    const currentVideo = videoRef.current;
+    if (currentVideo) {
+      const {currentTime} = currentVideo;
+      if ( Math.abs(currentTime - time) > 1) {
+        
+        currentVideo.currentTime = time;
+      }
+    }
+
+  }, [time])
+
+  const updateTime = useCallback((e) => onTimeUpdate(e.target.currentTime), [onTimeUpdate])
   if (format === "video") {
-    return <video {...props} />;
+    return <video ref={videoRef} onTimeUpdate={updateTime} {...props} />;
   }
   if (format === "image") {
-    return <img alt={alt || props.src} {...props} />;
+    return <img ref={imageRef} alt={alt || props.src} {...props} />;
   }
   return <div>PLACEHOLDER</div>;
 };
 
-const MediaPlayer = ({ src, format }) => {
+const Controls = ({ time, duration, onPlayClick, isPlaying, disableIsPlaying, onNextClick, disableNext, onPreviousClick, disablePrevious}) => {
+
+  const stringifiedDuration = stringifyTime(duration);
+  const stringifiedTime = stringifyTime(time);
+  
   return (
-    <div>
-      <Media format={format} src={src} />
+    <div className="media-player-controls">
+      <section className="media-player-controls-section flex-justify-start">
+        <time>{stringifiedTime} / {stringifiedDuration}</time>
+      </section>
+      <section className="media-player-controls-section flex-justify-center">
+        <button disabled={disableNext} onClick={onNextClick}>Next</button>
+        <button disabled={disableIsPlaying} onClick={onPlayClick}>{isPlaying ? "pause" : "play"}</button>
+        <button disabled={disablePrevious} onClick={onPreviousClick}>Prev</button>
+      </section>
+      <section className="media-player-controls-section flex-justify-end">
+      </section>
+    </div>
+  );
+}
+
+const Seeker = ({ time, duration, onSeek})=>{
+  const progressPercentile = time/duration * 100;
+  const onChange = (e) => {
+    const progressPercentile = e.target.value;
+    const time = progressPercentile * duration / 100;
+    onSeek(time);
+  };
+  return <input className="media-player-seeker" type="range" value={progressPercentile} min={0} max={100} step="0.001" onChange={onChange} />
+}
+
+const MediaPlayer = ({ src, format }) => {
+  const [isPlaying,setPlayingState] = useState(false);
+  const [time, setTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const togglePlayingState = () => setPlayingState(!isPlaying);
+  const handleTimeEvent = useCallback(setTime, [setTime])
+  const onDurationChange  = (e) => {
+    setDuration(e.target.duration)
+    setTime(0)
+  }
+  
+
+  return (
+    <div className="media-player">
+      <Media time={time} format={format} src={src} isPlaying={isPlaying} onTimeUpdate={handleTimeEvent} onEnded={togglePlayingState} onDurationChange={onDurationChange } />
+      <Seeker time={time} duration={duration} onSeek={setTime}/>
+      <Controls time={time} duration={duration} disableIsPlaying={format !== "video"} isPlaying={isPlaying} onPlayClick={togglePlayingState} onSeek={setTime}/>
     </div>
   );
 };
